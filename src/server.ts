d154c71c2fdf4aece511bdd1459abfd179aa4aa3
nealwp <mrprestonneal@gmail.com>
2023-01-04 21:53:32 -0800

    add healthcheck and initial root endpoint

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,14 @@ const PORT: number = 8081
 
 const server = express()
 
+server.get('/', (req, res, next) => {
+    res.json({message: 'hello friends'})
+})
+
+server.get('/health', (req, res, next) => {
+    res.status(200).send()
+})
+
 server.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`)
 })
