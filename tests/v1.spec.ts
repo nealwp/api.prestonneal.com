55262384bcc917558f00d19646348e165878e18d
nealwp <mrprestonneal@gmail.com>
2023-01-07 14:51:43 -0800

    add v1 tests

--- tests/v1.spec.ts
+++ tests/v1.spec.ts
@@ -3,24 +3,17 @@ import { createServer } from '../src/server'
 import { nullMiddleware } from '../src/middleware';
 import supertest from 'supertest';
 
-
-describe('default test', () => {
-    test('should pass', () => {
-        expect(true).toBe(true)
-    })
-});
-
 let server: Express;
 
 beforeAll(() => {
     server = createServer(nullMiddleware)
 })
 
-describe('api root', () => {
-    describe('GET /', () => {
+describe('api v1', () => {
+    describe('GET /v1/', () => {
         test('should return 200 & message object', async () => {
             await supertest(server)
-                .get(`/`)
+                .get(`/v1/`)
                 .expect(200)
                 .then((res) => {
                     expect(res.body).toMatchObject({ 'message': 'hello friends' })
@@ -28,11 +21,11 @@ describe('api root', () => {
         })
     })
     
-    describe('GET /health', () => {
+    describe('GET /v1/health', () => {
         test('should return 200', async () => {
             await supertest(server)
-                .get(`/health`)
+                .get(`/v1/health`)
                 .expect(200)
         })
     })
-})
+}) 
