cb6193d77982abdb62d1628f6fc6abade0477a7f
nealwp <mrprestonneal@gmail.com>
2023-01-08 14:27:31 -0800

    remove routes() calls on client routes

--- src/v1/routes/private/clients.routes.ts
+++ src/v1/routes/private/clients.routes.ts
@@ -1,20 +1,20 @@
 import { Router } from 'express'
 import { getClients, getClientById, createClient, deleteClient, updateClient } from '../../controllers/clients.controller'
 
-const router = Router()
+const clients = Router()
 
-router.route('/').get(async (req, res, next) => {
+clients.get('/', async (req, res, next) => {
     const clients = await getClients()
     res.json(clients)
 })
 
-router.route('/:id').get(async (req, res, next) => {
+clients.get('/:id', async (req, res, next) => {
     const id = req.params.id as unknown
     const client = await getClientById(id as number)
     res.json(client)
 })
 
-router.route('/create').post(async (req, res, next) => {
+clients.post('/', async (req, res, next) => {
     
     const {businessName, contactFirstName, contactLastName, email} = req.body
 
@@ -30,13 +30,13 @@ router.route('/create').post(async (req, res, next) => {
     
 })
 
-router.route('/:id').delete(async (req, res, next) => {
+clients.delete('/:id', async (req, res, next) => {
     const id = req.params.id as unknown
     await deleteClient(id as number)
     res.status(200).send()
 })
 
-router.route('/:id/update').post(async (req,res,next) => {
+clients.post('/:id', async (req,res,next) => {
     const { id } = req.params
     const {businessName, contactFirstName, contactLastName, email} = req.body
 
@@ -52,4 +52,4 @@ router.route('/:id/update').post(async (req,res,next) => {
     res.json(updatedClient)
 })
 
-export default router
+export default clients
