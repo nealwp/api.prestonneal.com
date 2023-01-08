e3f2f3baacbf8d27d4380a433f57b6ef4cdb621b
nealwp <mrprestonneal@gmail.com>
2023-01-08 11:25:06 -0800

    add bodyparser middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,9 +1,11 @@
 import { corsMiddleware } from './cors.middleware'
 import { loggerMiddleware } from './logger.middleware'
+import bodyParserMiddleware from './bodyparser.middleware'
 
 const middleware = [
     corsMiddleware,
-    loggerMiddleware
+    loggerMiddleware,
+    bodyParserMiddleware
 ]
 
 const nullMiddleware: any[] = [
